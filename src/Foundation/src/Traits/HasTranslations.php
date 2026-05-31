c3cc05c38bcf8013791450a6f6624cfd9293f987
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2026-06-01 00:44:25 +0300

    chore: add spatie/laravel-translatable package to composer.json and update composer.lock

---
+++ src/Foundation/src/Traits/HasTranslations.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Redot\Traits;
+
+use Spatie\Translatable\HasTranslations as BaseHasTranslations;
+
+trait HasTranslations
+{
+    use BaseHasTranslations;
+
+    /**
+     * Convert the model to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $attributes = $this->attributesToArray();
+
+        $translatables = array_filter($this->getTranslatableAttributes(), function ($key) use ($attributes) {
+            return array_key_exists($key, $attributes);
+        });
+
+        foreach ($translatables as $field) {
+            $attributes[$field] = $this->getTranslation($field, app()->getLocale());
+        }
+
+        return array_merge($attributes, $this->relationsToArray());
+    }
+}
